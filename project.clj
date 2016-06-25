325d7719215085bd6cd5f63601d98e18b65a2c51
Gary Fredericks <fredericksgary@gmail.com>
2016-06-25 17:52:18 -0500

    Setup lein releases

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/graphs "0.3.2"
+(defproject com.gfredericks/graphs "0.3.3-SNAPSHOT"
   :description "Undirected graph-theory clojure stuff."
   :url "https://github.com/fredericksgary/graphs"
   :license {:name "Eclipse Public License"
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.combinatorics "0.0.4"]
                  [potemkin "0.3.3"]]
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies [[criterium "0.4.2"]
                                   [org.clojure/test.check "0.9.0"]]}})
